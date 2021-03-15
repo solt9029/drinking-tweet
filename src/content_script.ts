7f3db6732a0a066bb827008e718bcc68f91e2119
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-03-15 21:37:15 +0900

    Refactor

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,46 +1,43 @@
 import * as $ from "jquery";
 
-$(function () {
+$(async function () {
   let port;
-  let sensorValue = 600;
+  const threshold = 600;
+  let sensorValue = threshold; // initial value
+  const buttonClassName = "css-901oao css-16my406 r-1tl8opc r-bcqeeo r-qvutc0";
 
-  (async function () {
-    try {
-      port = await (navigator as any).serial.requestPort();
-      await port.open({ baudRate: 9600 });
-      console.log("接続");
+  try {
+    port = await (navigator as any).serial.requestPort();
+    await port.open({ baudRate: 9600 });
+    console.log("connect");
 
-      while (port.readable) {
-        const reader = port.readable.getReader();
-        try {
-          while (true) {
-            const { value, done } = await reader.read();
-            const decodedValue = new TextDecoder().decode(value);
-
-            if (done) {
-              break;
-            }
-
-            console.log(decodedValue);
-            sensorValue = parseInt(decodedValue);
+    while (port.readable) {
+      const reader = port.readable.getReader();
+      try {
+        while (true) {
+          const { value, done } = await reader.read();
+          if (done) {
+            break;
           }
-        } catch (error) {
-          console.log(error);
-        } finally {
-          reader.releaseLock();
+          const decodedValue = new TextDecoder().decode(value);
+          console.log(decodedValue);
+          sensorValue = parseInt(decodedValue);
         }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        reader.releaseLock();
       }
-    } catch (e) {
-      console.log(e);
     }
-  })();
+  } catch (err) {
+    console.log(err);
+  }
 
   Array.from(document.getElementsByTagName("div")).forEach((element) => {
     element.addEventListener("click", function (event) {
       if (
-        (event.target as any).className ===
-          "css-901oao css-16my406 r-1tl8opc r-bcqeeo r-qvutc0" &&
-        sensorValue >= 600
+        (event.target as any).className === buttonClassName &&
+        sensorValue >= threshold
       ) {
         event.stopPropagation();
         window.open("https://solt9029.github.io/drinking-sudo/");
