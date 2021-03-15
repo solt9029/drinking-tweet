311167035d7a9e46fc56c060eda5767b742348b4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-03-15 20:40:51 +0900

    Use web serial api

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,13 +1,35 @@
 import * as $ from 'jquery';
 
 $(function() {
+    let port;
     (async function() {
         try {
-            const port = await (navigator as any).serial.requestPort();
+            port = await (navigator as any).serial.requestPort();
             await port.open({ baudRate: 9600 });
             console.log("接続");
+
+            while (port.readable) {
+                const reader = port.readable.getReader();
+                try {
+                  while (true) {
+                    const { value, done } = await reader.read();
+                    const decodedValue = (new TextDecoder).decode(value)
+                    
+                    if (done) {
+                      break;
+                    }
+                    
+                    console.log(decodedValue);
+                    // https://solt9029.github.io/drinking-sudo/
+                  }
+                } catch (error) {
+                  console.log(error);
+                } finally {
+                  reader.releaseLock();
+                }
+              }
           } catch (e) {
-            console.log("Error");
+            console.log(e);
           }
     })();
 });
