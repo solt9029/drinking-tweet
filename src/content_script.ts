ea1589327ed7560f83dbe3aca259fac0d655474b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-03-15 20:50:17 +0900

    Add prettier

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,35 +1,35 @@
-import * as $ from 'jquery';
+import * as $ from "jquery";
 
-$(function() {
-    let port;
-    (async function() {
+$(function () {
+  let port;
+  (async function () {
+    try {
+      port = await (navigator as any).serial.requestPort();
+      await port.open({ baudRate: 9600 });
+      console.log("接続");
+
+      while (port.readable) {
+        const reader = port.readable.getReader();
         try {
-            port = await (navigator as any).serial.requestPort();
-            await port.open({ baudRate: 9600 });
-            console.log("接続");
+          while (true) {
+            const { value, done } = await reader.read();
+            const decodedValue = new TextDecoder().decode(value);
 
-            while (port.readable) {
-                const reader = port.readable.getReader();
-                try {
-                  while (true) {
-                    const { value, done } = await reader.read();
-                    const decodedValue = (new TextDecoder).decode(value)
-                    
-                    if (done) {
-                      break;
-                    }
-                    
-                    console.log(decodedValue);
-                    // https://solt9029.github.io/drinking-sudo/
-                  }
-                } catch (error) {
-                  console.log(error);
-                } finally {
-                  reader.releaseLock();
-                }
-              }
-          } catch (e) {
-            console.log(e);
+            if (done) {
+              break;
+            }
+
+            console.log(decodedValue);
+            // https://solt9029.github.io/drinking-sudo/
           }
-    })();
+        } catch (error) {
+          console.log(error);
+        } finally {
+          reader.releaseLock();
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  })();
 });
